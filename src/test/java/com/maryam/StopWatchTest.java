a6d5e8d6a810ae10a3b90b40809cbbad4e1980d4
maryam tohidiyan <m.tohidiyan78@gmail.com>
2024-02-13 08:34:59 +0100

    some changes in Tests

--- src/test/java/com/maryam/StopWatchTest.java
+++ src/test/java/com/maryam/StopWatchTest.java
@@ -1,14 +1,27 @@
 package com.maryam;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StopWatchTest {
+
+    private StopWatch sw;
+    @BeforeEach
+    void createObjectOfStopWatch(){
+        sw = new StopWatch();
+    }
 
     @Test
     public void testFormatForGivenDuration() {
@@ -20,31 +33,22 @@ public class StopWatchTest {
         assertEquals("60 : 00 : 00", StopWatch.
                 format(Duration.between(now, now.plus(1, TimeUnit.HOURS.toChronoUnit()))));
     }
-   /* @Test
-    void testStart() {
-        StopWatch sw = new StopWatch();
-        sw.start();
-        Duration first = sw.calculateDuration();
-        Duration second = sw.calculateDuration();
-        assertNotEquals(first, second);
-    }*/
 
     @Test
     void testStart() {
-        StopWatch sw = new StopWatch();
         sw.start();
-        assertTrue(sw.isRunning());
-        assertFalse(sw.isFinished());
+        assertAll("start method test" ,
+                ()->{assertTrue(sw.isRunning());},
+                ()->{assertFalse(sw.isFinished());}
+                );
     }
     @Test
     void testStop() {
-        StopWatch sw = new StopWatch();
         sw.stop();
         assertFalse(sw.isRunning());
     }
     @Test
     void testReset(){
-        var sw = new StopWatch();
         sw.timeList.add(new TimesRecord(Instant.now(), StopWatch.Status.START));
         sw.reset();
         assertTrue(sw.isFinished());
@@ -54,7 +58,6 @@ public class StopWatchTest {
     }
     @Test
     void testNewLap(){
-        var sw = new StopWatch();
         sw.start();
         int firstSizeCheck = sw.listModelLapItems.size();
         sw.newLap();
@@ -62,21 +65,48 @@ public class StopWatchTest {
 
         assertEquals(1,secondSizeCheck-firstSizeCheck);
     }
-    @Test
-    void testAggregateGapsMethodWithGivenTimes(){
-        var sw= new StopWatch();
+ /*   @TestFactory
+    Collection<DynamicTest> testAggregateGapsMethodWithGivenTimes(){
         Instant now = Instant.now();
+        sw.timeList.add(new TimesRecord(now, StopWatch.Status.START));
         Instant stop = now.plusSeconds(1);
         Instant secondStart = now.plusSeconds(2);
-        sw.timeList.add(new TimesRecord(now, StopWatch.Status.START));
-        sw.timeList.add(new TimesRecord(stop , StopWatch.Status.STOP));
-        sw.timeList.add(new TimesRecord(secondStart , StopWatch.Status.START));
+        Instant secondStop = now.plusSeconds(3);
+        Instant thirdStart = now.plusSeconds(5);
+        return Arrays.asList(
+                DynamicTest.dynamicTest("one second stop" , ()->{
+                    sw.timeList.add(new TimesRecord(stop , StopWatch.Status.STOP));
+                    sw.timeList.add(new TimesRecord(secondStart , StopWatch.Status.START));
+                    assertEquals(1000 , sw.aggregateGaps().toMillis());
+                }),
+                DynamicTest.dynamicTest("two second stop" , ()->{
+                    sw.timeList.add(new TimesRecord(secondStop , StopWatch.Status.STOP));
+                    sw.timeList.add(new TimesRecord(thirdStart , StopWatch.Status.START));
+                    assertEquals(3000 , sw.aggregateGaps().toMillis());
+                })
+        );
+    }*/
 
-        assertEquals(1000 , sw.aggregateGaps().toMillis());
+    @ParameterizedTest
+    @ValueSource(ints = {2 , 3 , 4})
+    void testAggregateGapsMethodWithGivenTimes(int number ){
+        Instant now = Instant.now();
+        sw.timeList.add(new TimesRecord(now, StopWatch.Status.START));
+        Instant stop = now.plusSeconds(number);
+        sw.timeList.add(new TimesRecord(stop, StopWatch.Status.STOP));
+        Instant secondStart = stop.plusSeconds(number);
+        sw.timeList.add(new TimesRecord(secondStart, StopWatch.Status.START));
+        Instant secondStop = secondStart.plusSeconds(number);
+        sw.timeList.add(new TimesRecord(secondStop, StopWatch.Status.STOP));
+        Instant thirdStart = secondStop.plusSeconds(number);
+        sw.timeList.add(new TimesRecord(thirdStart, StopWatch.Status.START));
+
+        assertEquals(2L *number*1000 , sw.aggregateGaps().toMillis());
+
     }
-    @Test
-    void testCalculateDuration(){
-        var sw = new StopWatch();
+
+    @RepeatedTest(10)
+    void testCalculateDuration() {
         sw.start();
         Duration firstDurationCalculation= sw.calculateDuration();
         Duration secondDurationCalculation = sw.calculateDuration();
